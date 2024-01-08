1816207dcbf08fe11d0644f29311f515975a19e0
Aman Prajapati <pjaman05671@gmail.com>
2024-01-08 23:00:02 +0530

    added images

--- src/app/components/Project/Projecjson.js
+++ src/app/components/Project/Projecjson.js
@@ -3,18 +3,30 @@ export const Data = [
         index: '01',
         color:"bg-[#252525]",
         title: "LUMI",
-        description: "This is a project description",
+        description: "Ai powered story telling app",
         className: "top-0",
-        img:["/images/Lumi1.png","/images/Lumi2.png","/images/Lumi3.png","/images/Lumi4.png"],
+        img:["/images/Lumi1.png","/images/Lumi2.png","/images/Lumi3.png","/images/Lumi4.png"]
     },
-//    {
-//         index: '02',
-//         color:"bg-[#3460C7]",
-//         title: "PAX",
-//         description: "This is a project description",
-//         className:"top-0",
-//         img:["/images/Lumi1.png","/images/Lumi2.png","/images/Lumi3.png","/images/Lumi4.png"]
-//     },
+
+    {
+        index: '02',
+        color:"bg-[#327EF6]",
+        title: "NIRMAAN",
+        description: "College Club Website",
+        className:"top-0",
+        img:["/images/N1.png","/images/N2.png","/images/N3.png","/images/N4.png"],
+        
+        
+    },
+
+   {
+        index: '03',
+        color:"bg-[#473D99]",
+        title: "PAX",
+        description: "This is a project description",
+        className:"top-0",
+        img:["/images/E1.webp"]
+    },
 //     {
 //         index: '03',
 //         color:"bg-[#473D99]",
@@ -23,15 +35,6 @@ export const Data = [
 //         className:"top-0",
 //         img:["/images/Lumi1.png","/images/Lumi2.png","/images/Lumi3.png","/images/Lumi4.png"]
        
-//     },
-//     {
-//         index: '04',
-//         color:"bg-[#327EF6]",
-//         title: "NIRMAAN",
-//         description: "This is a project description",
-//         className:"top-0",
-//         img:["/images/Lumi1.png","/images/Lumi2.png","/images/Lumi3.png","/images/Lumi4.png"]
-        
 //     },
 //     {
 //         index: '05',
