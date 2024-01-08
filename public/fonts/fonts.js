b334e22aa76d57dc0c938bc73153fff600073067
Aman Prajapati <pjaman05671@gmail.com>
2024-01-08 21:14:53 +0530

    few changes

--- public/fonts/fonts.js
+++ public/fonts/fonts.js
@@ -2,5 +2,5 @@ import localFont from 'next/font/local'
 
 
 export const arges = localFont({ src: './arges-variable-wght-wdth-slnt.ttf' })
-export const bigS = localFont({ src: './bigShouldersDisplay-Regular.ttf' })
+
 export const PPmori = localFont({ src: './PPMori-Regular.otf' })
