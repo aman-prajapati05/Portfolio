8e4ef2dcae05e6550415ca5c98c82545fb35ff35
Aman Prajapati <pjaman05671@gmail.com>
2024-01-08 21:05:40 +0530

    few changes

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,6 +1,7 @@
 "use client"
 import React, { useEffect, useState } from 'react';
 import { PPmori,arges } from '../../../public/fonts/fonts';
+import { motion,useAnimation } from 'framer-motion';
 import Link from 'next/link';
 import './../globals.css'
 
@@ -20,15 +21,20 @@ export function Navbar() {
 
             <div className='flex flex-col'>
             <div  className={`${isActive ? 'flex justify-end pt-8 px-8':'flex justify-between h-full px-10 pt-8'}`}>
-                <div className={`${arges.className} font-black text-3xl ${isActive ? 'opacity-0' : ''}`}>PJ</div>
-                <div onClick={() => {setIsActive(!isActive)}} className='button z-[99]'>
+                <div className={`${arges.className} font-black text-3xl  ${isActive ? 'opacity-0' : ''}`}>PJ</div>
+                <div onClick={() => {setIsActive(!isActive)}} className='hover:scale-y-75 button z-[99]'>
                 <div className={`burger  ${isActive ? 'burgerActive ' : ' '}`}></div>
                 </div>
                 </div>
             
              {isActive && (
                 <> 
-                <div className='sticky top-4 -mt-28 ' >
+                <motion.div
+                initial={{ opacity: 0, y: '-100vh' }} 
+                animate={{ opacity: 1, y: 0 }} 
+                exit={{ opacity: 0, y: '-100vh' }} 
+                transition={{ duration: 0.5, ease: 'easeIn' }}
+                className='sticky top-4 -mt-28 ' >
                <div className='w-screen flex h-screen '>
                     <div className={`${arges.className} bg-white w-1/2 text-[#929598] tracking-wider z-50 hidden md:block`}>Hope you're having a great day ;)</div>
                     <div className={`${arges.className} text-[#A0A3A6] md:w-1/2 w-full h-full  bg-[#252C35] tracking-wider z-50`}>
@@ -42,9 +48,11 @@ export function Navbar() {
                             <div>/02</div>
                             <div className="text-5xl font-normal mons px-2 md:text-8xl">SKILLS</div>
                         </div>
-                        <div className="flex mt-4 text-xs md:text-base md:mt-16 md:justify-between hover:text-white">
+                        <div className="flex mt-4 text-xs md:text-base md:mt-16 md:justify-between hover:text-white overflow-hidden">
                             <div>/03</div>
-                            <div className="text-5xl font-normal mons px-2 md:text-8xl">WORK</div>
+                            <div 
+                            
+                            className="text-5xl font-normal mons px-2 md:text-8xl animate-on-hover">WORK</div>
                         </div>
                         <div className="flex mt-4 text-xs md:text-base md:mt-16 md:justify-between hover:text-white">
                             <div>/04</div>
@@ -54,7 +62,7 @@ export function Navbar() {
                     </div>
                     </div>
 
-               </div>
+               </motion.div>
                </>
             )} 
             
